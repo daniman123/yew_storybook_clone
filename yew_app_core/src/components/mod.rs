825df876527802f8f392af20cd827e67c2404225
daniman123 <daniel-saleh@hotmail.com>
2024-03-13 23:22:43 +0100

    test: added more testcomp cases

--- yew_app_core/src/components/mod.rs
+++ yew_app_core/src/components/mod.rs
@@ -10,3 +10,14 @@ pub fn TestComp(props: &TestCompProps) -> Html {
         <div> {"Testing Lib Comp"} </div>
     }
 }
+
+#[derive(PartialEq, Properties)]
+pub struct NewTestCompProps {}
+
+#[function_component]
+pub fn NewTestComp(props: &NewTestCompProps) -> Html {
+    let NewTestCompProps {} = props;
+    html! {
+        <div> {"Testing NewTestComp Lib Comp"} </div>
+    }
+}
